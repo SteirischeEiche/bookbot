e4a9b9d0c818abadc554313eb225deb92a9702f2
SteirischeEiche <210909647+SteirischeEiche@users.noreply.github.com>
2025-06-18 20:28:10 +0200

    L6: Count Characters

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ def main():
    text_string = get_book_text(book_path)
    num_words = count_words(text_string)
    print(f"{num_words} words found in the document")
+   dict_chars = count_chars(text_string)
+   print(dict_chars)
 
-def get_book_text(path):
-   with open(path) as f:
-      return f.read()
-
+from stats import get_book_text
 from stats import count_words
+from stats import count_chars
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def count_words(string):
    text_list = string.split()
    return len(text_list)
+
+def get_book_text(path):
+   with open(path) as f:
+      return f.read()
+
+def count_chars(string):
+   dic = {}
+   text_lower = string.lower()
+
+   for char in text_lower:
+      if char in dic:
+         dic[char] += 1
+      else:
+         dic[char] = 1
+
+   return dic
